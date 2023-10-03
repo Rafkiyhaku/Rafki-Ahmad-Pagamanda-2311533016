50700c2ecb501f2000c62262bd4d21241075e1ef
Ryan Al Ghifari <ryanalghifari.ra@gmail.com>
2023-10-03 09:20:53 +0700

    Praktikum ke 2

---
+++ praktikumAlpro2311533016/src/LuasPersegiPanjang.java
@@ -0,0 +1,9 @@
+
+public class LuasPersegiPanjang {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ praktikumAlpro2311533016/src/pekan2/OperatorLogika.java
@@ -0,0 +1,10 @@
+package pekan2;
+
+public class OperatorLogika {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
