087c43991810cb0c5cf2fc55f74331cdb2f46792
llenovo <llenovo@DESKTOP-088MGSE>
2023-10-11 22:15:22 +0700

    pekan3

---
+++ praktikumAlpro2311533016/src/pekan3/BilanganGenapGanjil.java
@@ -0,0 +1,23 @@
+package pekan3;
+
+import java.util.Scanner;
+
+public class BilanganGenapGanjil {
+	public static void main(String[] args) {
+		long noBP;
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("masukkan nomor BP anda");
+		noBP=input.nextLong();
+		input.close();
+		if(noBP%2 == 0) {
+			System.out.println("Merupakan bilangan genap");
+			
+		}else {
+			System.out.println("Merupakan bilangan ganjil");
+			
+		}
+
+	}
+
+}
